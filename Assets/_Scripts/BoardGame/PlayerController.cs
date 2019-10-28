ad0a864d9baec964017484ee982dbafe0a5f4c67
VirgileMezz <37793334+VirgileMezz@users.noreply.github.com>
2019-10-28 19:00:26 +0100

    debug mouvement board
    TOUT MARCHE PARFAITEMENT BIEN

--- Assets/_Scripts/BoardGame/PlayerController.cs
+++ Assets/_Scripts/BoardGame/PlayerController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private int currentCase = 0;
-    private int nextCase = 1;
+
+    private int currentCase = -1; //-1 car le debut de la partie ne commence pas sur une case
+    private int nextCase = 0; //0 car la la première case sera le premier déplacement
     private float moveSpeed = 1.0f;
     private int moveNumber = 0;
     // Start is called before the first frame update
